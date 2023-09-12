95a3fd7a46ef5fd9dba36498199197c9288f974d
geralm <estebanlm@estudiantec.cr>
2023-09-11 19:49:35 -0600

    event seeds

---
+++ seeds/events.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Event = require('../models/events');
+const {eventsNames, eventsDescriptions, getRandomDate} = require('./seedHelpers')
+mongoose.connect('mongodb://127.0.0.1:27017/gestion-de-anteproyectos', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)]; //choose a random data from the array in parameter
+const seedEvents = async()=>{
+    await Event.deleteMany({});
+    for(let i = 0; i<10; i++){  
+        const newEvent = new Event({
+            title: `${sample(eventsNames)}`,
+            description: `${sample(eventsDescriptions)}`,
+            startDate: getRandomDate(),
+            finishDate: getRandomDate()
+        })
+    }
+    console.log("Seeds added");
+}
+seedEvents().then(() => {
+    mongoose.connection.close();
+})
